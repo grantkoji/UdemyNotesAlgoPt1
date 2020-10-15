68702aa0c845b9045e379f16c40bd568ea028ac5
grantkoji <grantkoji@gmail.com>
2020-10-15 14:04:41 -0400

    with frequency counter

--- UdemyNotesAlgo.js
+++ UdemyNotesAlgo.js
@@ -59,8 +59,61 @@ function charCount(str) {
 }
 
 
+Section 5
+Frequency CountQueuingStrategyUsed Objects or 
+sets to collect values/frequencies of values
+This can often avoid the need for nested loops or Object(N^2) operations 
+with array/strings 
 
-dfafd
+Squared with same 
+Order can be mixed up, but want to have squared values 
+same([1, 2, 3], [4, 1, 9]) //true
+same([1, 2, 3], [1, 9]) //false
+same([1, 2, 3], [4, 4, 1]) //false 
+
+//naive solution with nested loops, indexOf is another loop
+function same(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    for (let i = 0; i < arr1.length; i++) {
+        let correctIndex = arr2.indexOf(arr1[i] ** 2)
+        //returns -1 if false
+        if (correctIndex === -1) {
+            return false;
+        }
+        arr2.splice(correctIndex, 1)
+    }
+    return true
+}
+
+//Refactored
+//2 separate loops is better than 1 nested loop
+function same(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    let frequencyCounter1 = {}
+    let frequencyCounter2 = {}
+    for (let val of arr1) {
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+    }
+    for (let val of arr2) {
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+    }
+    for (let key in frequencyCounter1) {
+        if (!(key ** 2 in frequencyCounter2)) {
+            return false
+        }
+        if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+            return false
+        }
+    }
+    return true
+}
+
+
+
 
 
 
